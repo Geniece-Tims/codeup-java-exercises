24f3165e13cdfae694a129e0dcd2b2f4433bd8f9
Geniece Tims <geniece.c.tims@gmail.com>
2023-06-13 14:53:21 -0500

    completed steps 1-3 of Syntax, Types, and Variables exercise for Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, World!");
+
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Dragon cohort is the best cohort.";
     }
+
 }
+
+
