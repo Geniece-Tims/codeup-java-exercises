8af91979ad0353986c03f423f174eb6e4a7a781b
Geniece Tims <geniece.c.tims@gmail.com>
2023-06-14 12:03:28 -0500

    completed bonus for Java Console IO exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,7 @@
 // has to be at the top of your code when using Scanner & InputMismatchException
 import java.util.InputMismatchException;
 import java.util.Scanner;
+//
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
@@ -8,6 +9,7 @@ public class ConsoleExercises {
 
 
         Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
         // Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
         try {
             System.out.println("Enter something: ");
@@ -43,6 +45,34 @@ public class ConsoleExercises {
         double perimeter = 2 * (length + width);
         System.out.println("Area: " + area);
         System.out.println("Perimeter: " + perimeter);
+
+        // BONUS:
+        System.out.println("Enter the length of your classroom: ");
+        double newLength = scanner.nextDouble();
+        System.out.println("Enter the width of your classroom: ");
+        double newWidth = scanner.nextDouble();
+        System.out.println("Enter the height of your classroom: ");
+        double newHeight = scanner.nextDouble();
+
+        double volume = newLength * newWidth * newHeight;
+        System.out.println("Volume: " + volume);
+
+        // Modifying the code to use nextInt() vs. nextLine()
+        System.out.println("Enter the length of your classroom: ");
+        int anotherLength = scanner.nextInt();
+        System.out.println("Enter the width of your classroom: ");
+        int anotherWidth = scanner.nextInt();
+        System.out.println("Enter the height of your classroom: ");
+        int anotherHeight = scanner.nextInt();
+
+        int newArea = anotherLength * anotherWidth;
+        int newPerimeter = 2 * (anotherLength + anotherWidth);
+        int newVolume = anotherLength * anotherWidth * anotherHeight;
+
+        System.out.println("New Area: " + newArea);
+        System.out.println("New Perimeter: " + newPerimeter);
+        System.out.println("New Volume: " + newVolume);
+
     }
 
 }
