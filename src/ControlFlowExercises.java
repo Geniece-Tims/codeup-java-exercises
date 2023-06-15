a1e1508eafb10711faf5f60ef896c7440e1bb6a9
Geniece Tims <geniece.c.tims@gmail.com>
2023-06-14 20:07:18 -0500

    Completed Java exercise with bonus for Control Statements and Loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -27,33 +27,34 @@ public class ControlFlowExercises {
         int countBackwards = 100;
         do {
             System.out.println(countBackwards);
-            countBackwards -=5;
+            countBackwards -= 5;
         } while (countBackwards >= -10);
 
-        // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+        // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000.
+        // Output should equal:
         int startsAt = 2;
         do {
             System.out.println(startsAt);
             startsAt *= 2;
-        } while (startsAt < 1000000 );
+        } while (startsAt < 1000000);
 
         // c. For
         // Refactor the previous two exercises to use a for loop instead.
 
         // First While Loop function using variable "j" since i has already been declared.
-        for(int j = 5; j <= 15; j++) {
+        for (int j = 5; j <= 15; j++) {
             System.out.println("j is " + j);
         }
 
-        for(int number = 0; number <= 100; number += 2) {
+        for (int number = 0; number <= 100; number += 2) {
             System.out.println(number);
         }
 
-        for(int backwardsByFive = 100; backwardsByFive >= -10; backwardsByFive -= 5) {
+        for (int backwardsByFive = 100; backwardsByFive >= -10; backwardsByFive -= 5) {
             System.out.println(backwardsByFive);
         }
 
-        for(int beginAt = 2; beginAt < 1000000; beginAt *= 2) {
+        for (int beginAt = 2; beginAt < 1000000; beginAt *= 2) {
             System.out.println(beginAt);
         }
 
@@ -63,7 +64,7 @@ public class ControlFlowExercises {
         //For the multiples of five: print “Buzz”.
         //For numbers which are multiples of both three and five: print “FizzBuzz”.
 
-        for(int fb = 1; fb <= 100; fb++) {
+        for (int fb = 1; fb <= 100; fb++) {
             if (fb % 3 == 0) {
                 System.out.println("Fizz");
             } else if (fb % 5 == 0) {
@@ -85,9 +86,74 @@ public class ControlFlowExercises {
         Scanner scanner = new Scanner(System.in);
         scanner.useDelimiter("\n");
 
-        System.out.println("Enter an integer: ");
-        String integerInput = scanner.nextLine();
+        String choice;
+        do {
+            System.out.println("Enter an integer: ");
+            int input = scanner.nextInt();
+            System.out.println("You entered: " + input);
 
+            System.out.println("Number\tSquare\tCube");
+            System.out.println("------\t------\t------");
+
+            for (int k = 1; k <= input; k++) {
+                int square = k * k;
+                int cube = k * k * k;
+                System.out.println(k + "\t" + square + "\t" + cube);
+            }
+            System.out.println("Would you like to continue? (yes/no): ");
+            choice = scanner.next();
+            if (!choice.equalsIgnoreCase("yes")) {
+            break;
+        }
+    } while (true);
+        // 4. Convert given number grades into letter grades.
+        //
+        //Prompt the user for a numerical grade from 0 to 100.
+        //Display the corresponding letter grade.
+        //Prompt the user to continue.
+        //Assume that the user will enter valid integers for the grades.
+        //The application should only continue if the user agrees to.
+        //Grade Ranges:
+        //
+        //A : 100 - 88
+        //B : 87 - 80
+        //C : 79 - 67
+        //D : 66 - 60
+        //F : 59 - 0
+
+        System.out.println("Enter a numeric grade between 0 and 100:");
+        int grade = scanner.nextInt();
+            if (grade >= 88 && grade <= 100) {
+                System.out.println("Your grade is an A");
+            } else if (grade >= 80 && grade <= 87) {
+                System.out.println("Your grade is a B");
+            } else if (grade >= 67 && grade <= 79) {
+                System.out.println("Your grade is a C");
+            } else if (grade >= 60 && grade <= 66) {
+                System.out.println("Your grade is a D");
+            } else {
+                System.out.println("Sorry, but you've gotten an F!");
+            }
+
+        System.out.println("Enter a numeric grade between 0 and 100:");
+        int newGrade = scanner.nextInt();
+            if(newGrade >= 95 && grade <= 100) {
+                System.out.println("Congratulations, you got an A+ !");
+            } else if (newGrade >= 90 && newGrade <= 94) {
+                System.out.println("Awesome job! You've got an A- !");
+            } else if (newGrade >= 85 && newGrade <= 89) {
+                System.out.println("Good Job! You got a B+ !");
+            } else if (newGrade >= 80 && newGrade <= 84) {
+                System.out.println("Sweet! You got a B- !");
+            } else if (newGrade >= 75 && newGrade <= 79) {
+                System.out.println("Not too bad! You got a C+ ");
+            } else if (newGrade >= 70 && newGrade <= 74) {
+                System.out.println("A C- isn't horrible, but try for a higher score next time!");
+            } else if (newGrade >= 60 && newGrade <= 69) {
+                System.out.println("A D isn't failing, but there's always room for improvement!");
+            } else {
+                System.out.println("I'm sorry to say, but you've earned an F! Let's try for better next time!");
+            }
+        }
     }
-}
 
